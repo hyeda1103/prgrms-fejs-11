fbf6d680f068eadfd706ebd35346745b98513a10
hyeda1103 <dalgona92@gmail.com>
2021-07-12 22:02:11 +0900

    Delete files that weren't written by me

---
+++ week02/dahyeKo/App.js
@@ -0,0 +1,51 @@
+function App({ $target, initialState }) {
+    this.$app = document.createElement('div')
+    this.state = initialState
+
+    $target.appendChild(this.$app)
+
+    const todoInput = new TodoInput({
+      $app: this.$app,
+      onAddTodo: (text) => {
+          console.log(this.state)
+        this.setState([
+          ...this.state,
+          {
+            text,
+            isCompleted: false,
+          },
+        ])
+      },
+    })
+
+    const todoList = new TodoList({        
+        $app: this.$app,
+        initialState: this.state,
+        onTodoClick: (clickedIdx) => {
+            const nextState = [...this.state]
+            nextState[clickedIdx].isCompleted = !nextState[clickedIdx].isCompleted
+            this.setState(nextState)
+        }
+    })
+
+    const todoCount = new TodoCount({
+      $app: this.$app,
+      initialState: this.state
+    })
+
+
+    this.setState = (nextState) => {
+      storage.save('todos', nextState)
+      this.state = nextState
+      this.render()
+    }
+
+    this.render = () => {
+      todoList.setState(this.state)
+      todoCount.setState(this.state)
+    }
+
+    window.addEventListener('remove-all', () => {
+        this.setState([])
+    })
+}
--- week02/roto/main.js
+++
@@ -1,6 +0,0 @@
-const $target = document.querySelector('main')
-
-new App({
-  $target,
-  initialState: storage.load('todos', [])
-})
